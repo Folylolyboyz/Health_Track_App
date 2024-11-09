b8f6dad8777f34da2c9da8a40d50623bf17772a2
Folylolyboyz <basak.nabajyoti.play@gmail.com>
2024-11-10 02:47:44 +0530

    Update: Basic interaction with MongoDB

--- database.py
+++ database.py
@@ -1,42 +1,50 @@
-from databaseConnection import client
 import requests
 import pymongo.synchronous.collection as mongoDatatypes
 
+from databaseConnection import client
+
 cl = client()
 
-columns = ["_id", 
-           "name", 
-           "gender", 
-           "height", 
-           "weight", 
-           "bmi", 
-           "smoking", 
-           "alcohol", 
-           "allergy"]
+# columns = ["_id", 
+#            "name", 
+#            "age", 
+#            "gender", 
+#            "height", 
+#            "weight", 
+#            "bmi", 
+#            "smoking", 
+#            "alcohol", 
+#            "allergy"]
 
-def insertUser(cl: mongoDatatypes.Collection, data:dict):
-    if userExists(cl, data["_id"]):
+def insertUser(data:dict):
+    if userExists(data["userid"]):
         print("User exists. Try updating.")
+        updateUser(data)
         return
     data["bmi"] = data["weight"] // ((data["height"]/100)**2)
     cl.insert_one(data)
+    print("User inserted")
 
-def updateUser(cl: mongoDatatypes.Collection, data:dict):
-    if not userExists(cl, data["_id"]):
+def updateUser(data:dict):
+    if not userExists(data["userid"]):
         print("User does not exists. Try inserting.")
         return
-    cl.update_one({"_id" : data["_id"]}, {"$set" : data})
-
+    cl.update_one({"userid" : data["userid"]}, {"$set" : data})
+    print("User updated")
 
 def getData(api: str) -> dict:
     data = dict(requests.get(api).json())
     return data
 
-def userExists(cl: mongoDatatypes.Collection, id: int) -> bool:
-    data = list(cl.find({"_id" : id}))
+def userExists(userid: int) -> bool:
+    data = list(cl.find({"userid" : userid}))
     if data:
         return True
     return False
 
-data = getData("https://run.mocky.io/v3/5e12de4f-77dd-4b04-abcc-3dae527e08f7")
-insertUser(cl, data)
+def getUserData(userid: int):
+    data = list(cl.find({"userid" : userid}))
+    return data
+
+# data = getData("https://run.mocky.io/v3/c3796c22-2916-4bc6-a57a-a62caf18fd2f")
+# updateUser(cl, data)
