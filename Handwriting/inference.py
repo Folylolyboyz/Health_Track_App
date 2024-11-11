16708dc4d90cc3a5fa3b1bfb7bb6208de96cb9bd
Folylolyboyz <basak.nabajyoti.play@gmail.com>
2024-11-12 00:22:01 +0530

    Update: Added spellcheck

--- Handwriting/inference.py
+++ Handwriting/inference.py
@@ -2,6 +2,7 @@ import onnxruntime as rt
 import numpy as np
 from PIL import Image
 from itertools import groupby
+from spellchecker import SpellChecker
 
 class InferenceModel:
     def __init__(self, path = "Handwriting/handwritingModel.onnx"):
@@ -23,6 +24,7 @@ class InferenceModel:
         preds = self.model.run(None , {self.input_name: converted_img})[0]
         text = self.ctc_decoder(preds, self.metadata["vocab"])[0]
         
+        text = self.spellcheck(text)
         del preds, self.model, self.input_shape, self.input_name, self.output_name, self.metadata
         return text
     
@@ -42,9 +44,25 @@ class InferenceModel:
         grouped_preds = [[k for k,_ in groupby(preds)] for preds in argmax_preds]
         text = ["".join([vocab[k] for k in group if k < len(vocab)]) for group in grouped_preds]
         return text
-
+    
+    def spellcheck(self, text: str):
+        spell = SpellChecker()
+        text = spell.correction(text)
+        del spell
+        return text
+    
 # Uncomment to test
 
-# model = InferenceModel()
-# img = Image.open("Handwriting/Dataset/b01-000-00-00.png").convert("RGB")
+model = InferenceModel()
+
+# Word
+# img = Image.open("Handwriting/Dataset/b01-000-00-05.png").convert("RGB")
 # print(model.predict(img))
+
+# Whole directory
+import os
+for i in os.listdir("Handwriting/Dataset/"):
+    model = InferenceModel()
+    print(f"Handwriting/Dataset/{i}")
+    img = Image.open(f"Handwriting/Dataset/{i}").convert("RGB")
+    print(model.predict(img))
