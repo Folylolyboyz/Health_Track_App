c3d50de4b9d8bf4e98f5d9b0d3a440b3efcd7c38
Folylolyboyz <basak.nabajyoti.play@gmail.com>
2024-11-11 05:33:59 +0530

    Update: Changed image transform

--- Covid19/inference.py
+++ Covid19/inference.py
@@ -2,15 +2,43 @@ import onnxruntime as rt
 import numpy as np
 from PIL import Image
 import torchvision
+import random
+
+class CustomTransform:
+    def __init__(self, size=(224, 224), mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], horizontal_flip=True):
+        self.size = size
+        self.mean = np.array(mean).reshape(3, 1, 1).astype(np.float32)
+        self.std = np.array(std).reshape(3, 1, 1).astype(np.float32)
+        self.horizontal_flip = horizontal_flip
+
+    def resize(self, img):
+        """Resize the image to the specified size."""
+        return img.resize(self.size, Image.BILINEAR)
+
+    def random_horizontal_flip(self, img):
+        """Randomly flip the image horizontally."""
+        if self.horizontal_flip and random.random() > 0.5:
+            return img.transpose(Image.FLIP_LEFT_RIGHT)
+        return img
+
+    def to_numpy_array(self, img):
+        """Convert a PIL Image to a numpy array in float32 format and scale pixel values to [0, 1]."""
+        img_array = np.array(img).astype(np.float32) / 255.0
+        return img_array.transpose((2, 0, 1))  # Change HWC to CHW
+
+    def normalize(self, array):
+        """Normalize a numpy array image with mean and standard deviation."""
+        return (array - self.mean) / self.std
+
+    def __call__(self, img):
+        """Apply all transformations in sequence to the image."""
+        img = self.resize(img)
+        img = self.random_horizontal_flip(img)
+        img_array = self.to_numpy_array(img)
+        return self.normalize(img_array).astype(np.float32)
 
 def onnxPredictData(imagereal, path = "Covid19/ViralOrCovid.onnx") -> np.int64:
-    test_transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(size=(224, 224)),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-    
-    
+    test_transform = CustomTransform(horizontal_flip=False)
     
     class_names = ['normal', 'viral', 'covid']
     # imagereal = Image.open(image).convert("RGB")
